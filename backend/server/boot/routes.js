5369f6586b23dff19135c3da884ac3ae119a0d4d
Jam Risser <jam@jamrizzi.com>
2017-07-08 14:34:17 -0500

    Added alexa endpoint

--- backend/server/boot/routes.js
+++ backend/server/boot/routes.js
@@ -1,7 +1,39 @@
 'use strict';
 
+var alexaApp = require('alexa-app');
+
+var alexa = new alexaApp.app('alexa');
+
 module.exports = (app) => {
-  app.get('/alexa', (req, res) => {
-    res.json({hello: 'world'});
+  alexa.express({
+    expressApp: app,
+    checkCert: false,
+    debug: process.env.NODE_ENV === 'development'
+  });
+
+  alexa.launch((req, res) => {
+    res.say('You launched the app');
+  });
+
+  alexa.dictionary = {
+    names: [
+      'matt',
+      'joe',
+      'bob',
+      'bill',
+      'mary',
+      'jane',
+      'dawn'
+    ]
+  };
+
+  alexa.intent('nameIntent', {
+    slots: { NAME: 'LITERAL' },
+    utterances: [
+      'my {name is|name\'s} {names|NAME}',
+      'set my name to {names|NAME}'
+    ]
+  }, (req, res) => {
+    res.say('Success!')
   });
 };

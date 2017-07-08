ad5272cd52d957a78c19b3024e2f9165cc870bbf
Jam Risser <jam@jamrizzi.com>
2017-07-08 18:50:37 -0500

    Added services

--- backend/server/alexa/luckyNumber.js
+++ backend/server/alexa/luckyNumber.js
@@ -1,12 +1,24 @@
 'use-strict';
 
+var _ = require('lodash');
+
 module.exports = {
   name: 'LUCKYNUMBERS',
   options: {
     slots: {}
   },
   controller: (req, res) => {
-    var number = Math.floor(Math.random() * 100);
-    res.say(`Your lucky number is ${number}`);
+    var m = server.models;
+    m.Prescription.find().then((prescriptions) => {
+      var prescriptionString = 'Your prescriptions are';
+      _.each(prescriptions, (prescription) => {
+        prescriptionString += ` ${prescription.name}`;
+      });
+      return prescriptionString;
+    }).then((prescriptionString) => {
+      res.say(prescriptionString);
+    });
+    // var number = Math.floor(Math.random() * 100);
+    // res.say(`Your lucky number is ${number}`);
   }
 };

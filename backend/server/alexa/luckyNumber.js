b418e3128ba51cf65e882eb82ba860537f2da48e
Jam Risser <jam@jamrizzi.com>
2017-07-08 19:30:03 -0500

    Fixed lucky number

--- backend/server/alexa/luckyNumber.js
+++ backend/server/alexa/luckyNumber.js
@@ -9,18 +9,8 @@ module.exports = (app) => {
       slots: {}
     },
     controller: (req, res) => {
-      var m = app.models;
-      m.Prescription.find().then((prescriptions) => {
-        var prescriptionString = 'Your prescriptions are';
-        _.each(prescriptions, (prescription) => {
-          prescriptionString += ` ${prescription.name}`;
-        });
-        return prescriptionString;
-      }).then((prescriptionString) => {
-        res.say(prescriptionString);
-      });
-      // var number = Math.floor(Math.random() * 100);
-      // res.say(`Your lucky number is ${number}`);
+      var number = Math.floor(Math.random() * 100);
+      res.say(`Your lucky number is ${number}`);
     }
   };
 };

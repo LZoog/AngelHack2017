5edb2ce31c511170cf3bb87ccbe2f3639beb6607
Jam Risser <jam@jamrizzi.com>
2017-07-08 21:16:03 -0500

    Added first crud operation

--- backend/server/services/answer.js
+++ backend/server/services/answer.js
@@ -1,5 +1,30 @@
 'use strict';
 
+var app = require('../server');
+var _ = require('lodash');
+
+var m = app.models;
+
 module.exports = {
-  createAnswer()
+  create(prescriptionName, questionName, dateAnswered, value) {
+    return m.Prescription.findOne({
+      where: {
+        name: prescriptionName
+      }
+    }).then((prescription) => {
+      if (!prescription) return {};
+      return m.Question.findOne({
+        where: {
+          prescription: prescription.id,
+          name: questionName
+        }
+      }).then((question) => {
+        if (!question) return {};
+        return question.answers.create({
+          dateAnswered: dateAnswered,
+          value: value
+        });
+      });
+    });
+  }
 };
--- backend/server/services/question.js
+++ backend/server/services/question.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var app = require('../server');
+var _ = require('lodash');
+var Err = require('err');
+
+var m = app.models;
+
 module.exports = {
 
 };

226c9a9b46ba062e2e85980a9e53487160e84be0
LZoog <lauren@zugai.com>
2017-07-09 09:46:13 -0500

    Chart w/fake data showing on prescription page

--- Ionic/src/pages/prescription/prescription.module.ts
+++ Ionic/src/pages/prescription/prescription.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { PrescriptionPage } from './prescription';
+import { Chart } from '../../components/chart/chart.component';
 
 @NgModule({
   declarations: [
     PrescriptionPage,
+    Chart
   ],
   imports: [
     IonicPageModule.forChild(PrescriptionPage),

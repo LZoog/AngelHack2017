c6f635ccf17a240087ce4f25188781241a4fd5e0
Jam Risser <jam@jamrizzi.com>
2017-07-09 11:57:49 -0500

    Added sockets

--- backend/server/services/socket.js
+++ backend/server/services/socket.js
@@ -1,7 +1,12 @@
 'use strict';
 
+var app = require('../server');
+var _ = require('lodash');
+
 module.exports = {
   fire: () => {
-
+    _.each(app.io.sockets.sockets, (socket, key) => {
+      socket.emit('screeam', {});
+    });
   }
 };
